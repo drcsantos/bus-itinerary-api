7fb3d67176438ba553ab81c89e1814dc2c9886e4
Diego Corrêa dos Santos <diego.correa@ab-inbev.com>
2019-11-01 12:47:31 -0300

    unplanned change: blocking updates for direction resource

--- src/routes/directions.js
+++ src/routes/directions.js
@@ -3,11 +3,11 @@ const directionService = require('../services/directions');
 const directionRoute = router => {
     router.get('/directions', (req, res, next) => {
         // TODO: BugFix for mobile, remove it
-        if (typeof req.query.enabled === 'undefined') {
+        /*if (typeof req.query.enabled === 'undefined') {
             if (!req.query.fields) {
                 req.query.fields = '-pathPoints,-wayPoints';
             }            
-        }
+        }*/
 
         directionService.getDirections(req.query)
             .then(data => res.send(data))
@@ -26,26 +26,29 @@ const directionRoute = router => {
     });
 
     router.post('/directions', (req, res, next) => {
-        directionService.addDirection(req.body)
+        res.status(401).end();
+        /*directionService.addDirection(req.body)
             .then(data => res.send(data))
-            .catch(next);
+            .catch(next);*/
     });
 
     router.put('/directions/:id', (req, res, next) => {
-        directionService.updateDirection(req.params.id, req.body)
+        res.status(401).end();
+        /*directionService.updateDirection(req.params.id, req.body)
             .then(data => {
                 if (data) {
                     return res.send(data);
                 }
                 return res.status(404).end();
             })
-            .catch(next);
+            .catch(next);*/
     });
 
     router.delete('/directions/:id', (req, res, next) => {
-        directionService.deleteDirection(req.params.id)
+        res.status(401).end();
+        /*directionService.deleteDirection(req.params.id)
             .then(data => res.status(data ? 200 : 404).end())
-            .catch(next);
+            .catch(next);*/
     });
 
     return router;
