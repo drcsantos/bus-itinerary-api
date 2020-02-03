4d61a1f250f69e676ddf3fa56dc6b7424cad5533
Diego Corrêa dos Santos <diego.correa@ab-inbev.com>
2020-02-02 23:12:27 -0300

    added accounts route

--- src/apiRouter.js
+++ src/apiRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const directionRoute = require('./routes/directions');
 
+const accountRoute = require('./routes/accounts');
 const userRoute = require('./routes/users');
 const productRoute = require('./routes/products');
 const paymentMethodRoute = require('./routes/paymentMethods');
@@ -9,6 +10,7 @@ const orderRoute = require('./routes/orders');
 const apiRouter = express.Router();
 
 directionRoute(apiRouter);
+accountRoute(apiRouter);
 userRoute(apiRouter);
 productRoute(apiRouter);
 paymentMethodRoute(apiRouter);
